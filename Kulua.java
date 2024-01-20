e8f212cc9135d49bab98e7f3f7bdcc9af8021208
Gaioz <tabtadzegaga@gmail.com>
2024-01-20 17:50:20 +0400

    pushy

---
+++ Kulua.java
@@ -0,0 +1,14 @@
+public class Kulua {
+
+    public static void main(String[] args) {
+        //my friend's name is Kulua, write some code for him
+        System.out.println("Hello Kulua");
+        // some guess the age app
+        int age = 20;
+        System.out.println("I guess your age is " + age);
+        // some guess the height app
+        double height = 1.8;
+        System.out.println("I guess your height is " + height);
+
+    }
+}
