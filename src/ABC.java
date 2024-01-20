aa0ef46fc08284fa43487d12fbff4a1249ef5cd6
Gaioz <tabtadzegaga@gmail.com>
2024-01-20 18:16:07 +0400

    pushy

---
+++ src/ABC.java
@@ -0,0 +1,16 @@
+package src;
+
+public class ABC {
+    public static void main(String[] args) {
+
+    }
+
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
