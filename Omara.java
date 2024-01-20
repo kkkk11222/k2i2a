8923daf37c5aff47383ebaf49eb21e2bc09961b5
Gaioz <tabtadzegaga@gmail.com>
2024-01-20 17:56:54 +0400

    pushy

---
+++ Omara.java
@@ -0,0 +1,7 @@
+public class Omara {
+    public static void main(String[] args) {
+        System.out.print("Hello, Omara!");
+        System.out.println("kai xar mara, mainc yle xar");
+    }
+    //
+}
